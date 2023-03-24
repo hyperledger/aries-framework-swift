db452ec72c68cc82695c079abd445d71cd5adb54
kukgini <kukgini@gmail.com>
2023-03-24 09:01:23 +0900

    Exclude walletKey from AgentConfig encode/decode. It should be stored separately in a safer place
    Signed-off-by: kukgini <kukgini@gmail.com>

---
+++ AriesFramework/AriesFrameworkTests/agent/AgentConfigTest.swift
@@ -0,0 +1,29 @@
+//
+//  AgentConfigTest.swift
+//  AriesFrameworkTests
+//
+//  Created by soominlee on 2023/03/23.
+//
+
+import Foundation
+import XCTest
+@testable import AriesFramework
+
+class AgentConfigTest: XCTestCase {
+    
+    /// all properties is codable except walletKey. walletKey must be secured.
+    func testCodable() async throws {
+        let walletKey = "whateverkey"
+        let genesisPath = "/path/to/genesis/file"
+        let config = AgentConfig(
+            walletKey: walletKey,
+            genesisPath: genesisPath)
+        UserDefaults.standard.setValue(try? PropertyListEncoder().encode(config), forKey:"testAgentConfig")
+        
+        if let data = UserDefaults.standard.value(forKey:"testAgentConfig") as? Data {
+            let config = try! PropertyListDecoder().decode(AgentConfig.self, from:data)
+            XCTAssertNil(config.walletKey)
+            XCTAssertEqual(config.genesisPath, genesisPath)
+        }
+    }
+}
