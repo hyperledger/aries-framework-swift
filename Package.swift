ed9f6ada5e952193ebd76235c611b7ae6ed51af5
conanoc <conanoc@gmail.com>
2024-02-29 15:21:31 +0900

    Update aries wrapper version to 0.2.0 (#80)
    Signed-off-by: conanoc <conanoc@gmail.com>

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
             targets: ["AriesFramework"])
     ],
     dependencies: [
-        .package(url: "https://github.com/hyperledger/aries-uniffi-wrappers", exact: "0.1.1"),
+        .package(url: "https://github.com/hyperledger/aries-uniffi-wrappers", exact: "0.2.0"),
         .package(url: "https://github.com/bhsw/concurrent-ws", exact: "0.5.0"),
         .package(url: "https://github.com/JohnSundell/CollectionConcurrencyKit", exact: "0.2.0"),
         .package(url: "https://github.com/keefertaylor/Base58Swift", exact: "2.1.7"),
